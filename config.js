d1df8f3a8c531fc9f8f01f020de876daa7f84a62
gerodiegarro <147152812+gerodiegarro@users.noreply.github.com>
2024-05-01 19:31:31 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,38 +1,36 @@
-
-
 const config = {}
 
 config.bot = {
 
     nodeMailer: [
         {
-            serviceEmail: '', //Your Gmail adress
-            password: '' //app password
+            serviceEmail: '', // Deine Email - sollte dafür eine Gmail sein
+            password: '' // App Password - Schau die die readme.md dazu an
         }
     ],
 
     bot: [
         {
-            token: '',
-            clientId: '',
-            guildId: '',
-            clientSecret: ''
+            token: '', // Dein Discord Bot Token - Erstelle hier einen Bot - https://discord.com/developers/
+            clientId: '', // Die Client ID von deinem Bot
+            clientSecret: '', // Die Client Secret von deinem Bot
+            guildId: '' // Die Server ID worauf sich der Bot befindet
+
         }
     ],
 
     allgemein: [
         {
-            teamrolle: '',
-            embedColor: '', //The Color of all Embeds
+            teamrolle: '', // Team Rollen ID die die Staff Commands ausführen dürfen
+            embedColor: '', // Die Hex Color die bei jedem Embed sich befindet (mit # davor)
         }
     ],
 
     KundenSystem: [
         {
-            kundenRolle: '', //Customer role
+            kundenRolle: '', // Die Rolle die der Benutzer bekommt wenn seine Kundennummer erstell wird
         }
     ],
 }
 
 module.exports = config
-
